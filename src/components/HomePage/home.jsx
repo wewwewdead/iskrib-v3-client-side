5d4d5ed1f3515e9e01c5debc035d23d69937fd04
Loren <johnmathewloren27@gmail.com>
2025-12-22 21:13:20 +0900

    fix bug

--- src/components/HomePage/home.jsx
+++ src/components/HomePage/home.jsx
@@ -40,7 +40,7 @@ const HomePage = () => {
         {
             path: '/home', 
             label: 'Home', 
-            action: ()=> navigatePath('/'), 
+            action: ()=> navigatePath('/home'), 
             icon: 
             <svg xmlns="http://www.w3.org/2000/svg" width="28px" height="28px" viewBox="0 0 24 24" fill="#000000ff">
                 <g id="style=fill">
