e53750135f0ab7c932417734b352766731d98216
Loren <johnmathewloren27@gmail.com>
2026-02-01 01:23:52 +0900

    fix some issues on bookmark toast

--- src/components/HomePage/postCards/PostCards.jsx
+++ src/components/HomePage/postCards/PostCards.jsx
@@ -384,16 +384,13 @@ const PostCards = () => {
                                 )}
                             </div>
                         </div>
-
                         {showBookmarkedMessage === journal.id && (
-                            <motion.div
+                            <div
                             className="bookmarked-content-message-container"
-                            initial={{opacity: 0, scale: 0.9}}
-                            animate={{opacity: 1, scale: 1}}
-                            transition={{type: 'spring', damping: 25, stiffness: 300, duration: 0.15}}
                             >
                                 {bookmarkedMessage}
-                            </motion.div>
+                                
+                            </div>
                         )}
                     </motion.div>
                 )
